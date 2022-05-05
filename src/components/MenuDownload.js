c817ad17abc2dc7baaef96511505c0572e14013e
Mark Howe <mvahowe@gmail.com>
2022-05-05 10:38:27 +0200

    Add stub content to download table

--- src/components/MenuDownload.js
+++ src/components/MenuDownload.js
@@ -1,9 +1,9 @@
 import React, {useState, useContext} from "react";
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow} from "@mui/material";
+import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Button} from "@mui/material";
 import i18n from '../lib/i18n';
 import AppLangContext from "../contexts/AppLang";
 
-export default function MenuDownload({}) {
+export default function MenuDownload() {
     const appLang = useContext(AppLangContext);
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(10);
@@ -17,107 +17,70 @@ export default function MenuDownload({}) {
         setPage(0);
     };
 
-/*    const tableRows = [
-        {org: "DCS", lang: `${i18n(appLang, 'language')}`, title: "Unfolding Word Literal Translation", versification: true},
-        {org: "Vachan", lang: `${i18n(appLang, 'language')}`, title: "Vachan Translation", versification: true},
-        {org: "eBible", lang: `${i18n(appLang, 'language')}`, title: "eBible Translation", versification: true},
-        {org: "Other", lang: `${i18n(appLang, 'language')}`, title: "Other Translation", versification: true},
-      ]*/
-      const columns = [
-        {
-            id: 'id1',
-            label: 'titulo1',
-            minWidth: 50,
-            align: 'right',
-            format: value => value ? "yes" : "no",
-        },
-        {
-            id: 'id2',
-            label: 'titulo2',
-            minWidth: 50,
-            align: 'right',
-            format: value => value ? "yes" : "no",
-        },
-        {
-            id: 'id3',
-            label: 'titulo3',
-            minWidth: 50,
-            align: 'right',
-            format: value => value ? "yes" : "no",
-        }
-      ];
-
-    const columna = [{
-        org: "DCS", 
-        lang: `${i18n(appLang, 'language')}`, 
-        title: "Unfolding Word Literal Translation", 
-        versification: true
-    }, 
-    {
-        org: "Vachan", 
-        lang: `${i18n(appLang, 'language')}`, 
-        title: "Vachan Translation", 
-        versification: true
-    }, 
-    {
-        org: "eBible", 
-        lang: `${i18n(appLang, 'language')}`, 
-        title: "eBible Translation", 
-        versification: true
-    }];
-    const rows = ['hola', 'estos', 'son', 'rows'];
+    const tableRows = [
+        {org: "DCS", lang: 'en', abbr: 'ult', title: "Unfolding Word Literal Translation"},
+        {org: "Vachan", lang: 'hi', abbr: 'vt', title: "Vachan Translation"},
+        {org: "eBible", lang: 'en', abbr: 'et', title: "eBible Translation"},
+        {org: "Other", lang: 'zzz', abbr: 'ot', title: "Other Translation"},
+    ];
 
     return <Paper sx={{width: '100%', overflow: 'hidden'}}>
-    <TableContainer sx={{maxHeight: 440}}>
-        <Table stickyHeader size="small" aria-label="sticky table">
-            <TableHead>
-                <TableRow>
-                    {columns.map((column) => (
-                        <TableCell
-                            key={column.id}
-                            align={column.align}
-                            style={{minWidth: column.minWidth}}
-                        >
-                            {column.label}
+        <TableContainer sx={{maxHeight: 440}}>
+            <Table stickyHeader size="small" aria-label="sticky table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            {i18n(appLang, 'organization')}
                         </TableCell>
-                    ))}
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {columna
-                    .map((row) => {
-                        return (
-                            <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                                {columns.map((column) => {
-                                    const value = row[column.id];
-                                    return (
-                                        <TableCell key={column.id} align={column.align}>
-                                            {
-                                                column.format &&
-                                                typeof value === 'boolean' &&
-                                                column.format(value)
-                                            }
-                                            {
-                                                !column.format && value
-                                            }
-                                        </TableCell>
-                                    );
-                                })}
-                            </TableRow>
-                        );
-                    })}
-            </TableBody>
-        </Table>
-    </TableContainer>
-    <TablePagination
-        siez="small"
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-    />
-</Paper>
+                        <TableCell>
+                            {i18n(appLang, 'language')}
+                        </TableCell>
+                        <TableCell>
+                            {i18n(appLang, 'abbreviation')}
+                        </TableCell>
+                        <TableCell>
+                            {i18n(appLang, 'title')}
+                        </TableCell>
+                        <TableCell>
+                            {i18n(appLang, 'download_actions')}
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {tableRows
+                        .map((row) => {
+                            return (
+                                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                                    <TableCell>
+                                        {row.org}
+                                    </TableCell>
+                                    <TableCell>
+                                        {row.lang}
+                                    </TableCell>
+                                    <TableCell>
+                                        {row.abbr}
+                                    </TableCell>
+                                    <TableCell>
+                                        {row.title}
+                                    </TableCell>
+                                    <TableCell>
+                                        <Button>To Do</Button>
+                                    </TableCell>
+                                </TableRow>
+                            );
+                        })}
+                </TableBody>
+            </Table>
+        </TableContainer>
+        <TablePagination
+            siez="small"
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={tableRows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+    </Paper>
 }
